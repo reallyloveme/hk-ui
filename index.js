a0b053382ce40f7bb3ca7a149d48c5fa89609a5f
宋君 <reallylovesj@126.com>
2021-06-12 16:47:53 +0800

    fix: 设置index

---
+++ index.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: your name
+ * @Date: 2021-06-12 16:46:17
+ * @LastEditTime: 2021-06-12 16:47:19
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /hk-common/Users/songjun/songjun/hk-ui/index.js
+ */
+import * as HKUi from './src/main'
+
+export default {
+    HKUi
+}
